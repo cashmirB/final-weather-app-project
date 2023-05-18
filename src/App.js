feafc51202f7b9600b9e5cedd483cffef295ba12
cashmirB <128428924+cashmirB@users.noreply.github.com>
2023-05-18 20:51:33 +0100

    added giturl

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
 
-function App() {
-  return <div className="App">Welcome To My Weather App</div>;
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Welcome To My Weather App</h1>
+      <footer>
+        <a
+          href="https://github.com/cashmirB/final-weather-app-project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open source code
+        </a>{" "}
+        by Cashmir Balajadia
+      </footer>
+    </div>
+  );
 }
-
-export default App;
