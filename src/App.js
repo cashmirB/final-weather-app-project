e900d8a6ee822b1c2b2a457ff25118cf01f8a944
cashmirB <128428924+cashmirB@users.noreply.github.com>
2023-05-18 21:08:14 +0100

    added bootstrap on index

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Welcome To My Weather App</h1>
-      <footer>
-        <a
-          href="https://github.com/cashmirB/final-weather-app-project"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Open source code
-        </a>{" "}
-        by Cashmir Balajadia
-      </footer>
+      <div className="container">
+        <h1>Welcome To My Weather App</h1>
+        <footer>
+          <a
+            href="https://github.com/cashmirB/final-weather-app-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open source code
+          </a>{" "}
+          by Cashmir Balajadia
+        </footer>
+      </div>
     </div>
   );
 }
